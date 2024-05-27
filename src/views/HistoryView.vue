b94772d2775e27b325a39c5b3bfd32aeac4693e8
VladimirWittich <vladimirwittich@web.de>
2024-05-27 23:48:29 +0200

    Anpassung für die Kommunikation mit der Datenbank

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -2,13 +2,32 @@
   <div class="container">
     <h4 class="profile-welcome">History Vladimir!</h4>
     <input type="text" v-model="searchQuery" placeholder="Search by name">
-    <ul>
-      <li v-for="item in filteredItems" :key="item.id">
-        {{ item.name }} - Sets: {{ item.sets }}, Repetitions: {{ item.repetitions }}, Weight: {{ item.weight }}, Total Weight: {{ item.totalWeight }}, Date: {{ formatDate(item.createdAt) }}
-      </li>
-    </ul>
+    <table>
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Sets</th>
+        <th>Repetitions</th>
+        <th>Weight</th>
+        <th>Total Weight</th>
+        <th>Date</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in sortedAndFilteredItems" :key="item.id">
+        <td>{{ item.name }}</td>
+        <td>{{ item.sets }}</td>
+        <td>{{ item.repetitions }}</td>
+        <td>{{ item.weight }}</td>
+        <td>{{ item.totalWeight }}</td>
+        <td>{{ formatDate(item.createdAt) }}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+
 
 <script setup lang="ts">
 import { ref, onMounted, computed } from 'vue';
@@ -28,6 +47,8 @@ interface ExerciseItem {
 const historyItems = ref<ExerciseItem[]>([]);
 const searchQuery = ref<string>('');
 
+
+
 // Abrufen der Daten beim Komponentenstart
 onMounted(async () => {
   try {
@@ -38,17 +59,29 @@ onMounted(async () => {
   }
 });
 
-// Filtern der Elemente basierend auf der Suchanfrage
-const filteredItems = computed(() => {
-  return historyItems.value.filter(item => item.name.toLowerCase().includes(searchQuery.value.toLowerCase()));
+// Filtern und Sortieren der Elemente basierend auf der Suchanfrage
+const sortedAndFilteredItems = computed(() => {
+  return historyItems.value
+      .filter(item => item.name.toLowerCase().includes(searchQuery.value.toLowerCase()))
+      .sort((a, b) => {
+        // Zuerst nach Name sortieren
+        const nameComparison = a.name.localeCompare(b.name);
+        if (nameComparison !== 0) return nameComparison;
+
+        // Dann nach Datum sortieren
+        return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+      });
 });
 
 // Formatieren des Datums
 function formatDate(dateString: string): string {
   const date = new Date(dateString);
-  return date.toLocaleString(); // Customize this if you need a different format
+  return date.toLocaleDateString(); // Gibt nur das Datum zurück
 }
+
+
 </script>
+
 
 <style scoped>
 .container {
