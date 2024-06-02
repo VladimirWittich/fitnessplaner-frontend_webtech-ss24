0f156fca222d9cd3d879bb7973debf58e29813fe
VladimirWittich <vladimirwittich@web.de>
2024-06-02 16:02:51 +0200

    Anpassungen für okta

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ import UserProfileView from "@/views/UserProfileView.vue";
 
 // Router-Definition
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BACKEND_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
   {
     path: '/',
