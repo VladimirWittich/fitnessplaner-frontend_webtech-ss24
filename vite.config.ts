c1245b9462d555d4702f4962da88bd3fd1a762e3
VladimirWittich <vladimirwittich@web.de>
2024-06-30 21:39:49 +0200

    Render-Anpassungen

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,6 +13,20 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        fallback: fileURLToPath(new URL('./200.html', import.meta.url))
+      }
+    }
+  },
+  server: {
+    fs: {
+      strict: true
     }
   }
 })
