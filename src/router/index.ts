4fd6080d9d283a7239bd62e2d2715d9570c38ee8
VladimirWittich <vladimirwittich@web.de>
2024-06-30 22:05:29 +0200

    Router-Anpassungen

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,8 @@ import BMICalculator from "@/views/BMICalculator.vue";
 
 // Router-Definition
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
+  linkActiveClass: 'active',
   routes: [
     {
       path: '/',
